277b0462f9a59700d1e4c607e963ff4e70e65173
admeeer <alexpeltier@hotmail.com>
2023-10-10 16:29:58 -0700

    rename test_script func, rename .yml file

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,7 +1,7 @@
 # test_script.py
 import subprocess
 
-def test_hello_world_output(capfd):
+def test_script_output(capfd):
     subprocess.run(['python', './script.py'])
     
     # Capture the output
