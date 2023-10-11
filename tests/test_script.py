d254b0a32956f4e594ad0ddba84cf4c818936ae7
admeeer <alexpeltier@hotmail.com>
2023-10-11 13:59:08 -0700

    ensure script and test_script use same interpreter

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,9 +1,10 @@
 # test_script.py
 import subprocess
+import sys
 
 def test_script_output(capfd):
-    # run the script file under a new process
-    subprocess.run(['python', './script.py'])
+    # Run the script file under a new process using the same Python interpreter
+    subprocess.run([sys.executable, './script.py'])
     
     # Capture process output
     captured = capfd.readouterr()
